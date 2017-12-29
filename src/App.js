c26f61973401a59d7b980ecdca1438efcd591190
warlyware <danward@gmail.com>
2017-12-28 20:17:51 -0500

    Add basic globe with lakes geojson data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import 'aframe';
+import 'aframe-geojson-component';
 import 'aframe-particle-system-component';
-import {Entity, Scene} from 'aframe-react';
+import * as GeoJSONData from './assets/data.geojson';
+import { Entity, Scene } from 'aframe-react';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 
+console.log(GeoData);
 class App extends Component {
   render() {
     return (
       <Scene>
-        <Entity>
-          <a-entity camera="active: true; userHeight: 1.6" position="0 0 0"></a-entity>
-        </Entity>
-        <Entity geometry={{primitive: 'box', width: 5}} position="0 0 -5"/>
+        <a-assets>
+          <a-asset-item id="json-lakes-50m" src={GeoJSONData} />
+        </a-assets>
+        <Entity
+         camera={{
+           active: true,
+           position:{
+            x: 0, y: 0, z: 0
+           }
+          }}
+        />
+        <Entity
+          rotate='-1'
+          scale='2 2 2'
+          geojson={{
+            src: '#json-lakes-50m',
+            featureKey: 'name',
+            pointScale: 0.0000001,
+            pointAs: 'bar',
+            pointSizeFeature: 'pop_min',
+            lineWidth: 4,
+            pointScaling: 'linear'
+          }}
+          geometry={{
+            primitive: 'sphere',
+            radius: 1
+          }}
+          material={{
+            color: 'red'
+          }}
+          position={{
+            x: 0, y: 0, z: -5}}/>
       </Scene>
     );
   }
